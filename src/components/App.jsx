718c453c53c76b7558d30bfeea8065aa489b738e
ashuddeveloper <73248786+ashuddeveloper@users.noreply.github.com>
2022-05-03 00:55:25 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function inputHandler(event) {
+    const { value, name } = event.target;
+    setContact((prevValue) => {
+      if (name === "fName") {
+        return { fName: value, lName: prevValue.lName, email: prevValue.email };
+      } else if (name === "lName") {
+        return { lName: value, fName: prevValue.fName, email: prevValue.email };
+      } else if (name === "email") {
+        return { email: value, fName: prevValue.fName, lName: prevValue.lName };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={inputHandler}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={inputHandler}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={inputHandler}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
